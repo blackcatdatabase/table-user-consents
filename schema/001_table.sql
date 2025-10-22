92b2b6cd5909ae5e35510d765366aafe8dc5fce4
Tolben <vitek.pasek@gmail.com>
2025-10-22 21:00:42 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/001_table.sql
+++ schema/001_table.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: user_consents
 CREATE TABLE IF NOT EXISTS user_consents (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
--- schema/020_indexes.sql
+++ schema/020_indexes.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 (map@1e83bb6)
+-- Auto-generated from schema-map.psd1 (map@6cefe8e)
 -- table: user_consents
 CREATE INDEX idx_user_consents_user ON user_consents (user_id);
