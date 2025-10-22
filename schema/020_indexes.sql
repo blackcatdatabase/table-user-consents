afdac3d2d448eda4e3dd89fc9b4251248d11e0fd
Tolben <vitek.pasek@gmail.com>
2025-10-22 02:20:43 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/020_indexes.sql
+++ schema/020_indexes.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
+-- Auto-generated from schema-map.psd1 (map@1e83bb6)
 -- table: user_consents
 CREATE INDEX idx_user_consents_user ON user_consents (user_id);
